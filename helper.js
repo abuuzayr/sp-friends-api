8ef148d9bae25b0953d8b9a5861885f9e737cf2a
Abu Uzayr <abu.uzayr@builtforfifty.com>
2018-09-10 16:56:59 +0800

    Add default helper message

--- helper.js
+++ helper.js
@@ -1,5 +1,8 @@
 let helper = {
     error: (res, msg) => {
+        msg = 'string' === typeof msg 
+            ? msg 
+            : 'An unknown error occurred';
         res.send({
             error: true,
             error_msg: msg
