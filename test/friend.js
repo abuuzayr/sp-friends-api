f1048ae118a2945a457ec79f4d42b789a75eec65
Abu Uzayr <abu.uzayr@builtforfifty.com>
2018-09-11 06:04:48 +0800

    Add failing /new route test

--- test/friend.js
+++ test/friend.js
@@ -22,12 +22,32 @@ describe('Friends', () => {
         });
     });
 
+    // test base route
     describe('test friend api', () => {
         it('it should GET 200', (done) => {
             chai.request(server)
                 .get('/friends')
                 .end((err, res) => {
                     res.should.have.status(200);
+                    done();
+                });
+        });
+    });
+
+    // test new friend fail
+    describe('test /friends/new to fail creation of new friend', () => {
+        it('it should not create a friend without an email field', (done) => {
+            let friend = {
+                title: "not an email",
+            }
+            chai.request(server)
+                .post('/friends/new')
+                .send(friend)
+                .end((err, res) => {
+                    res.should.have.status(500);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('error');
+                    res.body.should.have.property('error_msg').eql('Invalid JSON body sent');
                     done();
                 });
         });
