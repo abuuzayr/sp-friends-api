0ebb660fff25ac4de35565693f6f13c232eee1b7
Abu Uzayr <abu.uzayr@builtforfifty.com>
2018-09-10 16:00:43 +0800

    Add and initialise bodyParser

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // initialise dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // initialise constants
 const app = express();
 const port = process.env.PORT || 8670;
 const router = express.Router();
+
+// setup app to use body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // set up API routes
 router.get('/', function (req, res) {
